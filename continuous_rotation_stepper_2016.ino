91f942141c8dcb0dc6b89496d644e89c496ca4cc
venya krutikov <venya@thekazimier.co.uk>
2016-02-22 13:24:00 +0000

    initial code

---
+++ continuous_rotation_stepper_2016.ino
@@ -0,0 +1,77 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <OSCBundle.h>
+#include "EulerStepCounter.h"
+#include <AccelStepper.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED } ;
+byte ip[] = { 192, 168, 1, 100 };
+int serverPort = 10000; // (incoming port)
+
+AccelStepper stepper1(4, 2, 3, 5, 6);
+long val1 = 0;
+
+EthernetUDP Udp;
+EulerStepCounter stepCounter;
+
+
+
+void setup() {
+  Serial.begin(250000);
+  Ethernet.begin(mac, ip);
+  Udp.begin(serverPort);
+  stepper1.setMaxSpeed(500.0);
+  stepper1.setAcceleration(200.0);
+
+}
+
+void loop() {
+
+
+  float reading = parseOSCMessage();
+
+  if( reading != -1.0 )
+    stepCounter.update(reading);
+
+
+  Serial.print("Euler:   ");
+  Serial.println(reading);
+  Serial.print("Stepper: ");
+  Serial.println(stepCounter.linear_position());
+
+  delay(50);
+}
+
+// either returns the euler angle or -1.0 if packet not received
+float parseOSCMessage() {
+  OSCMessage msgIn;
+  int size;
+
+  if ((size = Udp.parsePacket()) > 0) {
+    while (size--)
+      msgIn.fill(Udp.read());
+      msgIN.route("/compass/2",funcStepper1);
+    return(msgIn.getFloat(0));
+  } else {
+    return(-1.0);
+  }
+}
+
+void funcStepper1(OSCMessage &msg, int addrOffset ){
+
+  float value = stepCounter.linear_position()
+  val1 = (long)(value*200);
+  OSCMessage msgOUT("/compass/2");
+  stepper1.moveTo(val1);
+  //Serial.print("Value = : ");
+  //Serial.println(value);
+
+  msgOUT.add(val1);
+
+  Udp.beginPacket(Udp.remoteIP(), destPort);
+  msgOUT.send(Udp); // send the bytes
+  Udp.endPacket(); // mark the end of the OSC Packet
+  msgOUT.empty(); // free space occupied by message
+}
+
